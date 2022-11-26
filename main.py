9716a5a28ad75c201993e44fbb627dd299637529
thatjam <119136453+thatjam@users.noreply.github.com>
2022-11-26 13:28:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-import html
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Richard's personal site</title>
+</head>
+
+<body>
+    <img src="https://gamingtoptens.com/wp-content/uploads/2020/01/etf-1.jpg">
+    <h1>Richard Rosettie</h1>
+    <p><em>Founder and CEO of <strong>tarkov</strong>.</em></p>
+    <p>I am a gamer and I love playing tarkov my game</p>
+    <hr>
+    <h3>games I've played</h3>
+    <ul>
+        <li>tarkov</li>
+        <li>cs-go</li>
+    </ul>
+    <h3>Gaming Experience</h3>
+    <table>
+        <tr>
+            <td>2015-current time</td>
+            <td>I have been playing CS-GO since 2015</td>
+        </tr>
+        <td>2019-2022</td>
+        <td>I have been playing rust since 2019</td>
+    </table>
+    <hr>
+
+</body>
+</html>
 
 
